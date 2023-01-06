6458d48e09af47b836013cbb23ad8844e90728fb
Harrison <harrison831123@gmail.com>
2023-01-06 14:18:15 +0800

    第一次

---
+++ MyCreation/MyCreation/DBService/DBService.cs
@@ -0,0 +1,44 @@
+﻿using EPBKS.Db;
+using MyCreation.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace MyCreation.DBService
+{
+    public class DBService
+    {
+        DbHelperSql tssql = null;
+        /// <summary>
+        /// 檢查會議時間有無重複
+        /// </summary>
+        public MeetingDetail ChkMeetingTime(int MDID, DateTime SDate, DateTime EDate)
+        {
+            tssql = new DbHelperSql("H2O");
+            MeetingDetail result = new MeetingDetail();
+            string sql = @"with t1 as(select * from H2O.dbo.MeetingOrder)";
+            sql += " select * from t1,MeetingDevice D,Meeting A";
+            sql += " WHERE t1.MDID = D.MDID and t1.MTID = A.MTID and ((t1.MOStartDate <= @MOStartDate AND t1.MOEndDate >=  @MOStartDate) or (t1.MOStartDate <=  @MOEndDate AND t1.MOEndDate >= @MOEndDate)";
+            sql += " or (t1.MOStartDate >= @MOStartDate AND t1.MOEndDate <= @MOEndDate))";     
+            sql += " and t1.MDID = @MDID";
+
+            tssql.sqlParaColl.Clear();
+            tssql.sqlParaColl.AddWithValue("@MDID", MDID);
+            tssql.sqlParaColl.AddWithValue("@SDate", SDate);
+            tssql.sqlParaColl.AddWithValue("@EDate", EDate);
+            IDataReader rdr = tssql.SQL_ExecReader(sql);
+
+            while (rdr.Read())
+            {
+                result.MDID = (int)rdr["MDID"];
+            }
+            rdr.Close();
+            tssql.SQL_Close();
+
+
+            return result;
+        }
+    }
+}
---
+++ MyCreation/MyCreation/Models/MeetingDetail.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyCreation.Models
+{
+    public class MeetingDetail
+    {
+        public int MOID { get; set; }
+
+        public int MDID { get; set; }
+
+        public string imember { get; set; }
+
+        public string iunit { get; set; }
+
+        public string MOCreateIP { get; set; }
+
+        public string MOTitle { get; set; }
+
+        public string MODesc { get; set; }
+
+        public DateTime MOCreateDate { get; set; }
+
+        public DateTime MOStartDate { get; set; }
+
+        public DateTime MOEndDate { get; set; }
+
+        public int MOLendState { get; set; }
+
+        public int MOCheck { get; set; }
+
+        public int MTID { get; set; }
+
+        public string MTName { get; set; }
+
+        public string MDName { get; set; }
+
+        public string nmember { get; set; }
+
+        public string nunit { get; set; }
+
+        public string iaccount { get; set; }
+    }
+}
